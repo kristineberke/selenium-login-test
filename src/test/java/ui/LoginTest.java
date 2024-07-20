febaeb2ddb32069a3437fb4b65cff2dbbc04d241
Kristine Berke <kristine.berke@gmail.com>
2024-07-20 22:54:22 +0300

    Updated login test case

--- src/test/java/ui/LoginTest.java
+++ src/test/java/ui/LoginTest.java
@@ -1,19 +1,33 @@
 package ui;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
+	public static String browser = "chrome";
+	public static WebDriver driver;
 
 	public static void main(String[] args) {
+		if (browser.equals("firefox")) {
 
-		// 1.step - do the set up of the webdriver (chrome driver)
-		WebDriverManager.chromedriver().setup();
+			// 1.step - do the set up of the webdriver (chrome driver)
+			WebDriverManager.firefoxdriver().setup();
 
-		// 2.step - create the object of the chrome driver
-		ChromeDriver driver = new ChromeDriver(); // --> if it shows error - import Chrome Driver
+			// 2.step - create the object of the chrome driver
+			driver = new FirefoxDriver();
+
+		} else if (browser.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		} else if (browser.equals("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		}
 
 		// 3. step - open the browser
 		driver.get("https://www.saucedemo.com/v1/");
